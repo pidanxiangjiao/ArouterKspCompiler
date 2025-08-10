ea5f31aa59b8d299135f89a6efa9f189b46f6cf5
cuikangyuan <cuikangyuan@shein.com>
2025-08-10 23:11:21 +0800

    add brfieldinfo

---
+++ baseLibrary/src/main/java/androidx/databinding/BRFieldInfo.java
@@ -0,0 +1,14 @@
+package androidx.databinding;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface BRFieldInfo {
+    String name();
+    int value();
+}
